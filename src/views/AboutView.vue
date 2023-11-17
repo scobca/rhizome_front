fb40c6e8bc9943d5c7a1310e5e7138b9a6483a82
Artem622 <artemiiko2014@mail.ru>
2023-11-17 22:53:00 +0300

    create small components

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,3 +3,16 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+</script>
